3a9e695e775cee7c11eb08c91caaeb9b4a4654c1
Artur Futerko <32021534+futr0@users.noreply.github.com>
2020-12-08 14:58:17 +0100

    UI for lobby
    UI for lobby,
    Prefabs

--- Assets/Scripts/NetworkManagerShooter.cs
+++ Assets/Scripts/NetworkManagerShooter.cs
@@ -13,52 +13,56 @@ public class NetworkManagerShooter : NetworkManager
 
     public static event Action OnClientConnected;
     public static event Action OnClientDisconnected;
+    public string SceneLobbyName;
 
     public override void OnStartServer() => spawnPrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs").ToList();
 
-	public override void OnStartClient()
-	{
+    public override void OnStartClient()
+    {
         var spawnablePrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs");
 
-        foreach(var prefab in spawnablePrefabs)
-		{
+        foreach (var prefab in spawnablePrefabs)
+        {
             ClientScene.RegisterPrefab(prefab);
-		}
-
+        }
     }
 
-	public override void OnClientConnect(NetworkConnection conn)
-	{
-		base.OnClientConnect(conn);
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
         OnClientConnected?.Invoke();
-	}
+    }
 
-	public override void OnClientDisconnect(NetworkConnection conn)
-	{
-		base.OnClientDisconnect(conn);
-		OnClientDisconnected?.Invoke();
-	}
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
 
-	public override void OnServerConnect(NetworkConnection conn)
-	{
-		if (numPlayers >= maxConnections)
-		{
-			conn.Disconnect();
-			return;
-		}
-		if (SceneManager.GetActiveScene().name != menuScene)
-		{
-			conn.Disconnect();
-			return;
-		}
-	}
+        OnClientDisconnected?.Invoke();
+    }
 
-	public override void OnServerAddPlayer(NetworkConnection conn)
-	{
-		if (SceneManager.GetActiveScene().name == menuScene)
-		{
-			NetworkRoomPlayerShooter roomPlayerInstance = Instantiate(roomPlayerPrefab);
-			NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
-		}
-	}
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        if (numPlayers >= maxConnections)
+        {
+            conn.Disconnect();
+            return;
+        }
+
+        if (SceneManager.GetActiveScene().name != SceneLobbyName)
+        {
+            conn.Disconnect();
+            return;
+        }
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        if (SceneManager.GetActiveScene().name == SceneLobbyName)
+        {
+            NetworkRoomPlayerShooter roomPlayerInstance = Instantiate(roomPlayerPrefab);
+
+            NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
+        }
+    }
 }
